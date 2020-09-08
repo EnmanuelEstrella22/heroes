44a03491609d1ebb67adbaf6c0beca72f703e24d
Jose Enmanuel Estrella <enmanuelgit@gmail.com>
2020-09-08 11:40:43 -0400

    Updates

---
+++ precache-manifest.0ebf13b37dc909820aec4a522422becb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5546388e327672fcd83c7dc492bd128b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5d0af935d8634d49706d",
+    "url": "/static/js/2.f0832daf.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.f0832daf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c516f9b9e6d99534dc26",
+    "url": "/static/js/main.80eda1a5.chunk.js"
+  },
+  {
+    "revision": "94d7ca25d4df19621824",
+    "url": "/static/js/runtime-main.b7f813af.js"
+  }
+]);
